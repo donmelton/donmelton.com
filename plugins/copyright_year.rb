895ab564d077503281e4b715982a0afc8de4142a
Don Melton <don@blivet.com>
2013-01-02 21:53:45 -0800

    Change simple dash to en dash.

--- plugins/copyright_year.rb
+++ plugins/copyright_year.rb
@@ -4,7 +4,7 @@ def copyright_year(starting_year)
   current_year = Time.now.strftime("%Y")
 
   if starting_year.to_i < current_year.to_i
-    starting_year.to_s + '-' + current_year
+    starting_year.to_s + '&ndash;' + current_year
   else
     current_year
   end
